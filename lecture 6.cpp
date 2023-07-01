ad0fc0b40a7fe0cd361f7d35fcd765e8385ad4f3
shalusingh456 <136883953+shalusingh456@users.noreply.github.com>
2023-07-01 23:49:15 +0530

    Add files via upload

---
+++ lecture 6.cpp
@@ -0,0 +1,31 @@
+ 
+ //lec 6iii
+ #include<iostream>
+ using namespace std;
+ int main()
+{
+	int i;
+
+
+ 	i=1; //initialization
+ 	
+	 
+ 	while(i<101) //condition
+ 	{
+ 		cout<<i<<" ";   //loop
+ 		i++;          // update
+ 	
+ 	 int i;
+ 	 int input;
+ 	
+ 	
+ 	//gues the number 65
+ 	//the user has 100 change
+ 	
+ 	i = 100;
+ 	for(i=1;i<101;i++)
+ {
+ 		cin>>input;
+ //	if(input == 65)   
+
+}
