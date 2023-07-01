182c9d3991e792ec800f31555ca3e6252e3cfd23
shalusingh456 <136883953+shalusingh456@users.noreply.github.com>
2023-07-01 23:48:41 +0530

    Add files via upload

---
+++ lecture4.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+/*	int marks;
+	cout<<"Student, please enter the marks to calculate the grade :";
+	cin>>marks;
+	if(marks>90)
+	{
+		cout<<"A";
+	}
+	else(marks<90 && marks>80);
+	{
+	
+        cout<<"B";
+	}
+	if(marks<80 && marks>70)
+{
+
+        cout<<"C";
+	}
+	else(marks<70);
+	{
+		cout<<"Pass";
+	}
+}*/
+
+
+int a;
+/*cout<<"Give three integer as input: a,b,c"<<endl;
+cin>>a>>b>>c>>d;
+if(a>b && c>b) 
+{
+	cout<<"haha";
+}
+else if(a>b && c<d)
+{
+	cout<<"hehe";
+	
+}
+else(a<b && c>d);
+{
+cout<<"huhu";*/
+cout<<"enter a integer a"<<endl;
+cin>>a;
+switch(a) // top the condition in this parenthesis //it will first evaluate first evaluate the value pf a
+{
+	case 1:
+		cout<<"The value of a is 1"<<endl;
+		break;
+		case 2:
+			break;
+			cout<<"The value of a is 2"<<endl;
+			case 3:
+			cout<<"The value of a is 2"<<endl;
+			default:
+				cout<<"default will  be always printed";
+				break;
+					
+}
+}
+
+
+
